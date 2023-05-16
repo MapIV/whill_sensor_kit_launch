ecd7f2515d7c4756407059d93bf567ac6e132904
urasakikeisuke <keisuke.urasaki@map4.jp>
2023-05-16 16:06:22 +0900

    update params for whill

--- common_sensor_launch/launch/velodyne_node_container.launch.py
+++ common_sensor_launch/launch/velodyne_node_container.launch.py
@@ -142,8 +142,8 @@ def launch_setup(context, *args, **kwargs):
             plugin="pointcloud_preprocessor::DistortionCorrectorComponent",
             name="distortion_corrector_node",
             remappings=[
-                ("~/input/twist", "/sensing/vehicle_velocity_converter/twist_with_covariance"),
-                ("~/input/imu", "/sensing/imu/imu_data"),
+                ("~/input/twist", "/whill/twist_with_covariance"),
+                ("~/input/imu", "/imu/data_raw"),
                 ("~/input/pointcloud", "mirror_cropped/pointcloud_ex"),
                 ("~/output/pointcloud", "rectified/pointcloud_ex"),
             ],
--- sample_sensor_kit_launch/launch/pointcloud_preprocessor.launch.py
+++ sample_sensor_kit_launch/launch/pointcloud_preprocessor.launch.py
@@ -35,9 +35,7 @@ def launch_setup(context, *args, **kwargs):
         parameters=[
             {
                 "input_topics": [
-                    "/sensing/lidar/top/outlier_filtered/pointcloud",
-                    "/sensing/lidar/left/outlier_filtered/pointcloud",
-                    "/sensing/lidar/right/outlier_filtered/pointcloud",
+                    "/sensing/lidar/velodyne/outlier_filtered/pointcloud",
                 ],
                 "output_frame": LaunchConfiguration("base_frame"),
             }
